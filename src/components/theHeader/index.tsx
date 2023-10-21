d776e97fe5bc0a9ca68d1ed9058ed974fd8f95ce
drafolin <derg@drafolin.ch>
2023-10-21 13:26:32 +0200

    added header

--- src/components/theHeader/index.tsx
+++ src/components/theHeader/index.tsx
@@ -1,6 +1,14 @@
+import clubIcon from "@/assets/images/icon.png";
+import "./style.scss";
+
 export const Component = () => {
 	return <header>
-
+		<img src={clubIcon} className="icon" alt="L'icône du club" />
+		<div className="menu-button">
+			<span />
+			<span />
+			<span />
+		</div>
 	</header>;
 };
 
