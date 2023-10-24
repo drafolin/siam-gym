ccee2db588e7b6c9056bd4aa86d12859022432f4
drafolin <derg@drafolin.ch>
2023-10-24 18:19:30 +0200

    started site

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import {
 	createBrowserRouter,
 	RouterProvider,
 } from "react-router-dom";
+import "normalize.css";
 
 const router = createBrowserRouter([
 	{
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,15 @@
 import './index.scss';
+import clubIcon from '@/assets/images/icon.png';
 
 export const Component = () => {
 	return (
-		<>
-
-		</>
+		<main>
+			<section className="intro">
+				<img src={clubIcon} alt="Icône de Siam Gym" />
+				<p>Bienvenue au</p>
+				<h1>SiamGym</h1>
+			</section>
+		</main>
 	);
 };
 
