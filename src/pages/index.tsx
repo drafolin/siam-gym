061d5e9e4a88f8795a54cd1e3eb0b6ff3938d420
Dråfølin <derg@drafolin.ch>
2023-10-24 22:39:50 +0200

    added prestation list

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,21 @@ export const Component = () => {
 				<p>Bienvenue au</p>
 				<h1>SiamGym</h1>
 			</section>
+			<section className="offers">
+				<h2>Nos prestations</h2>
+				<ul>
+					<li>Boxe thaïlandaise (tous niveaux)</li>
+					<li>Boxe thaïlandaise (enfants)</li>
+					<li>Boxe anglaise (tous niveaux)</li>
+					<li>Circuit training <ul>
+						<li>Renforcement musculaire</li>
+						<li>Cardio</li>
+					</ul></li>
+					<li>Coaching privé</li>
+					<li>Programme d’entraînement personnalisé</li>
+					<li>Préparation physique</li>
+				</ul>
+			</section>
 		</main>
 	);
 };
