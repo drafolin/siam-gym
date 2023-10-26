694df61fff148f749d30d341e13cfa2034fc090d
drafolin <derg@drafolin.ch>
2023-10-26 21:12:44 +0200

    fixed animation on button

--- src/components/theHeader/index.tsx
+++ src/components/theHeader/index.tsx
@@ -2,22 +2,42 @@ import clubIcon from "~/assets/images/icon.png";
 import "./style.scss";
 import { useSize } from "ahooks";
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { CSSTransition } from "react-transition-group";
+
+enum MenuState {
+	OPENING,
+	OPENED,
+	CLOSING,
+	CLOSED
+}
 
 export const Component = () => {
 	const size = useSize(document.body);
-	const [isMenuOpen, setIsMenuOpen] = useState<boolean | null>(null);
-	const [currentMenuState, setCurrentMenuState] = useState<boolean>(false);
+	const [menuState, setMenuState] = useState<MenuState>(MenuState.CLOSED);
+	const animation = useRef<number | null>(null);
 	const isBigScreen = () => ((size?.width ?? 0) > 768);
 
 	const toggleMenu = () => {
-		console.log("toggleMenu");
-		setIsMenuOpen(null);
-		requestAnimationFrame(() => {
-			setIsMenuOpen(!currentMenuState);
-			setCurrentMenuState(!currentMenuState);
-		});
+		if (menuState === MenuState.OPENED || menuState === MenuState.OPENING) {
+			if (animation.current) {
+				clearTimeout(animation.current);
+			}
+
+			setMenuState(MenuState.CLOSING);
+			animation.current = setTimeout(() => {
+				setMenuState(MenuState.CLOSED);
+			}, 300);
+		} else if (menuState === MenuState.CLOSED || menuState === MenuState.CLOSING) {
+			if (animation.current) {
+				clearTimeout(animation.current);
+			}
+
+			setMenuState(MenuState.OPENING);
+			animation.current = setTimeout(() => {
+				setMenuState(MenuState.OPENED);
+			}, 300);
+		}
 	};
 
 	return <>
@@ -32,13 +52,28 @@ export const Component = () => {
 					</ul>
 				</nav> :
 				<div className="menu-button" onClick={toggleMenu}>
-					<span className={isMenuOpen === null ? "" : isMenuOpen ? "opened" : "closed"} />
-					<span className={isMenuOpen === null ? "" : isMenuOpen ? "opened" : "closed"} />
-					<span className={isMenuOpen === null ? "" : isMenuOpen ? "opened" : "closed"} />
+					<span className={
+						menuState === MenuState.CLOSED ? "" :
+							menuState === MenuState.CLOSING ? "closing" :
+								menuState === MenuState.OPENING ? "opening" :
+									menuState === MenuState.OPENED ? "open" : ""
+					} />
+					<span className={
+						menuState === MenuState.CLOSED ? "" :
+							menuState === MenuState.CLOSING ? "closing" :
+								menuState === MenuState.OPENING ? "opening" :
+									menuState === MenuState.OPENED ? "open" : ""
+					} />
+					<span className={
+						menuState === MenuState.CLOSED ? "" :
+							menuState === MenuState.CLOSING ? "closing" :
+								menuState === MenuState.OPENING ? "opening" :
+									menuState === MenuState.OPENED ? "open" : ""
+					} />
 				</div>}
 		</header>
 		<CSSTransition
-			in={!isBigScreen() && currentMenuState}
+			in={!isBigScreen() && (menuState === MenuState.OPENED || menuState === MenuState.OPENING)}
 			timeout={350}
 			classNames="menu"
 			unmountOnExit>
