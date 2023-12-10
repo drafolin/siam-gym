0fe8241bb9e9d116ad3a32d272cdeedf6af226e3
drafolin <derg@drafolin.ch>
2023-12-10 18:51:12 +0100

    Added specific title, added description

--- src/pages/cours/index.tsx
+++ src/pages/cours/index.tsx
@@ -1,49 +1,50 @@
 import "./index.scss";
 
 export const Page = () => {
-    return <main className="page-cours">
-        <section className="intro">
-            <h1>Nos cours</h1>
+  return <main className="page-cours">
+    <section className="intro">
+      <h1>Nos cours</h1>
+    </section>
+    <section className="planning">
+      <h2>Les horaires</h2>
+      <table></table>
+    </section>
+    <section className="prices">
+      <h2>Les tarifs</h2>
+      <h3>Nos abonnements</h3>
+      <div className="subs">
+        <section>
+          <h4>Muay Thaï</h4>
+          <p>4 entrainements par semaine</p>
+          <table>
+            <thead>
+            <tr>
+              <th></th>
+              <th>1 mois</th>
+              <th>dès 3 mois</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+              <td>Enfant</td>
+              <td>20.-</td>
+              <td>20.-/mois</td>
+            </tr>
+            <tr>
+              <td>Etudiant</td>
+              <td>55.-</td>
+              <td>50.-/mois</td>
+            </tr>
+            <tr>
+              <td>Adulte</td>
+              <td>65.-</td>
+              <td>60.-/mois</td>
+            </tr>
+            </tbody>
+          </table>
         </section>
-        <section className="planning">
-            <h2>Les horaires</h2>
-            <table></table>
-        </section>
-        <section className="prices">
-            <h2>Les tarifs</h2>
-            <h3>Nos abonnements</h3>
-            <div className="subs">
-                <section>
-                    <h4>Une discipline</h4>
-                    <table>
-                        <thead>
-                        <tr>
-                            <th></th>
-                            <th>1 mois</th>
-                            <th>dès 3 mois</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <tr>
-                            <td>Enfant</td>
-                            <td>20.-</td>
-                            <td>20.-/mois</td>
-                        </tr>
-                        <tr>
-                            <td>Etudiant</td>
-                            <td>55.-</td>
-                            <td>50.-/mois</td>
-                        </tr>
-                        <tr>
-                            <td>Adulte</td>
-                            <td>65.-</td>
-                            <td>60.-/mois</td>
-                        </tr>
-                        </tbody>
-                    </table>
-                </section>
 
-                {/*
+        {/*
 			<hr/>
 			<h4>Full access</h3>
 			<table>
@@ -74,29 +75,29 @@ export const Page = () => {
 			</table>
 
 			*/}
-            </div>
-            <h3>Cours uniques</h3>
-            <p>
-                Vous pouvez participer à un seul cours pour le prix de 10.- par cours.
-            </p>
-            <h3>Carte multicours</h3>
-            <p>
-                Si vous souhaitez participer à plusieurs cours, nous offrons des cartes
-                de fidélité, qui offrent une réduction de 10.- sur une tranche de 10 cours.
-            </p>
-            <p>
-                Ainsi, vous payez 90.- pour 10 cours.
-            </p>
-        </section>
-        <section>
-            <h2>Cours sur demande</h2>
-            <ul>
-                <li>Coaching privé ou équipe</li>
-                <li>Programme d'entraînement personnalisé</li>
-                <li>Préparation physique complète</li>
-            </ul>
-        </section>
-    </main>;
+      </div>
+      <h3>Cours uniques</h3>
+      <p>
+        Vous pouvez participer à un seul cours pour le prix de 10.- par cours.
+      </p>
+      <h3>Carte multicours</h3>
+      <p>
+        Si vous souhaitez participer à plusieurs cours, nous offrons des cartes
+        de fidélité, qui offrent une réduction de 10.- sur une tranche de 10 cours.
+      </p>
+      <p>
+        Ainsi, vous payez 90.- pour 10 cours.
+      </p>
+    </section>
+    <section>
+      <h2>Cours sur demande</h2>
+      <ul>
+        <li>Coaching privé ou équipe</li>
+        <li>Programme d'entraînement personnalisé</li>
+        <li>Préparation physique complète</li>
+      </ul>
+    </section>
+  </main>;
 };
 
 export default Page;
